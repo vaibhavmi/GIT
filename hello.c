a2f8021124b97da860d2f73255189e0e2d489907
Vaibhav Mittal <vaibhavmittalaln@gmail.com>
2018-03-04 02:40:42 +0530

    Edit hello.c by user_2

--- hello.c
+++ hello.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+void microkernel_sendmsg(char *);
+
 void main(){
 	printf("Helloworld!\n");
+	microkernel_sendmsg("is moreportable");
+}
+
+void microkernel_sendmsg(char *a){
+	printf("microkernel: %s\n", a);
 }
